af883fb8985ba20442f52e83d34f057eadf09d30
Grzegorz Nowak <strange3studio@gmail.com>
2017-12-30 20:29:20 +0100

    work on parsing json

--- src/json_dryer/core.clj
+++ src/json_dryer/core.clj
@@ -4,21 +4,22 @@
     [clojure.data.json :as json]
     [clojure.tools.cli :refer [parse-opts]]))
 
+
 (def cli-options
   [["-s" "--source-files VALUE" "source files pattern"                          :default nil]
    ["-t" "--target-file VALUE"  "target file to store abstracted commonalities" :default nil]])
 
-(def mkw
-  println)
+(defn extract-json
+  [file]
+  (json/read-str (slurp file) :key-fn keyword))
 
 (defn get-commonalities
   [files]
-  (map (fn [file] ((comp println json/read-str) (slurp file) :key-fn keyword)) files))
+  (map (fn [file] (extract-json file) files)))
 
 (defn write-commonalities
   [commonalities]
   (println commonalities))
-
 
 (defn invalid?
   [commonality]
@@ -26,9 +27,11 @@
 
 (defn -main [& args]
   (let [parsed-options (:options (parse-opts args cli-options))
-        validated-commonalities ((comp (partial remove invalid?) get-commonalities glob)
-                                 (:source-files parsed-options))]
+        commonalities  ((comp get-commonalities glob) (:source-files parsed-options))
+        _              (println commonalities)]))
+        ;  validated-commonalities ((comp (partial remove invalid?)))
+       ;   _ (println validated-commonalities)]))
 
 
-     (doall (write-commonalities validated-commonalities))))
+  ;   (doall (write-commonalities validated-commonalities))))
 
